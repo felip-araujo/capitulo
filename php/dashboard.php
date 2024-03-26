896547ee2d2fd96ed211e26ed539d20a0de039e6
felip-araujo <felipearaujoexecutive@gmail.com>
2024-03-26 15:58:49 -0400

    ala

--- php/dashboard.php
+++ php/dashboard.php
@@ -19,11 +19,23 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <title>CI 2024</title>
 </head>
 <body> 
-    <h1>Bem-vindo ao Capítulo Inspetorial 2024</h1>
+    <div class="header">
+
+        <h1 class="h3">Bem-vindo ao Capítulo Inspetorial 2024 </h1>
+    </div>
     <p>Conteúdo protegido. Apenas usuários autenticados podem acessar esta página.</p>
-    <p><a href="logout.php">Sair</a></p>
+    <p><a href="logout.php"i class="fa-solid fa-arrow-right-from-bracket"></a></p>
 </body>
+
+<script async src="https://kit.fontawesome.com/d4755c66d3.js" crossorigin="anonymous"></script>
+
+<!-- CSS do Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- JavaScript do Bootstrap (opcional, mas necessário para funcionalidades como dropdowns, modals, etc.) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
