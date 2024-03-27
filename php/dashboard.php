f909b5a4365affef7e55785275cf9b43f05cb1bd
felip-araujo <felipearaujoexecutive@gmail.com>
2024-03-27 11:15:14 -0400

    dashboard 70%

--- php/dashboard.php
+++ php/dashboard.php
@@ -1,13 +1,13 @@
 <?php
 
 // Iniciar a sessão
-session_start(); 
+session_start();
 
 
 // Verificar se o usuário está autenticado
 if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
     // Se não estiver autenticado, redirecionar para a página de login
-    echo "<script>alert('Usuário não autenticado!')</script>"; 
+    echo "<script>alert('Usuário não autenticado!')</script>";
     echo "<script>window.location.href = '../index.html';</script>";
     exit;
 }
@@ -16,6 +16,7 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,13 +24,53 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <title>CI 2024</title>
 </head>
-<body> 
-    <div class="header">
+
+<body>
+    <div class="header2">
 
         <h1 class="h3">Bem-vindo ao Capítulo Inspetorial 2024 </h1>
     </div>
-    <p>Conteúdo protegido. Apenas usuários autenticados podem acessar esta página.</p>
-    <p><a href="logout.php"i class="fa-solid fa-arrow-right-from-bracket"></a></p>
+
+
+    <div class="row mt-5">
+        <div class="col-md-6">
+            <div class="area">
+                <h2 class="h3">Fotos</h2>
+                <p class="p">Acompanhe as Fotos do Capítulo Inspetorial</p>
+                <?php
+                // Listar todos os arquivos JPG no diretório de destino
+                $diretorio_destino = '../files/';
+                $arquivos = scandir($diretorio_destino);
+                foreach ($arquivos as $arquivo) {
+                    if ($arquivo != '.' && $arquivo != '..' && pathinfo($arquivo, PATHINFO_EXTENSION) == 'jpg') {
+                        echo '<img src="' . $diretorio_destino . $arquivo . '" alt="' . $arquivo . '" class="img-thumbnail mb-3">';
+                    }
+                }
+                ?>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="area">
+                <h2 class="h3">Documentos</h2> 
+                <p class="p">Clique no documento abaixo para abrir o PDF</p>
+                <?php
+                // Listar todos os arquivos PDF no diretório de destino
+                foreach ($arquivos as $arquivo) {
+                    if ($arquivo != '.' && $arquivo != '..' && pathinfo($arquivo, PATHINFO_EXTENSION) == 'pdf') {
+                        echo '<a href="' . $diretorio_destino . $arquivo . '" class="btn btn-primary mb-3 ">' . $arquivo . '</a><br>';
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+    <a href="logout.php" i class="fa-solid fa-arrow-right-from-bracket"></a>
 </body>
 
 <script async src="https://kit.fontawesome.com/d4755c66d3.js" crossorigin="anonymous"></script>
@@ -39,4 +80,5 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
 
 <!-- JavaScript do Bootstrap (opcional, mas necessário para funcionalidades como dropdowns, modals, etc.) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </html>
--- php/upload.php
+++ php/upload.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <title>Envio de Arquivos</title>
 </head>
+
 <body>
     <h2>Enviar Arquivos</h2>
-    <form action="processar_upload.php" method="post" enctype="multipart/form-data">
-        <label for="imagem">Upload de Imagem (JPEG):</label>
-        <input type="file" name="imagem" accept="image/jpeg"><br><br>
 
-        <label for="pdf">Upload de PDF:</label>
-        <input type="file" name="pdf" accept="application/pdf"><br><br>
-
-        <input type="submit" value="Enviar Arquivos">
-    </form>
+    <div class="row mt-5">
+        <div class="col-md-6">
+            <div class="area">
+                <form action="processar_upload.php" method="post" enctype="multipart/form-data" class="login-container">
+                    <label class="form-label" for="imagem">Upload de Imagem (JPEG):</label>
+                    <input class="form-control" type="file" name="imagem" accept="image/jpeg"><br><br>
+                    <label class="form-label" for="pdf">Upload de PDF:</label>
+                    <input type="file" name="pdf" accept="application/pdf"><br><br>
+                    <input type="submit" value="Enviar Arquivos" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
