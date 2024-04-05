b0d5d253b05ad5792e0941dbc0d40f7f4c83004b
felip-araujo <felipearaujoexecutive@gmail.com>
2024-04-05 15:06:06 -0400

    g

--- php/dashvotos.php
+++ php/dashvotos.php
@@ -24,7 +24,7 @@
             <div class="col-md-6 mb-3 text-md-end">
                 <a href="voto_delegado.php" class="btn btn-outline-dark">Delegado CG29</a>
                 <a class="btn btn-outline-dark me-2" href="alterar-senha.html">Alterar minha senha</a>
-                <a href="" class="btn btn-outline-dark" onclick="window.print()">Imprimir Resultados</a>
+                <!-- <a href="" class="btn btn-outline-dark" onclick="window.print()">Imprimir Resultados</a> -->
             </div>
         </div>
 
