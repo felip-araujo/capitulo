95b9053ecc0bf1b156717c8645c056c6b733db39
felip-araujo <felipearaujoexecutive@gmail.com>
2024-04-05 14:33:37 -0400

    changes

--- php/dashvotos.php
+++ php/dashvotos.php
@@ -21,7 +21,7 @@
             <div class="col-md-6 mb-3">
                 <a class="btn btn-outline-dark" href="dashboard.php">Voltar</a>
             </div>
-            <div class="col-md-6 mb-3 text-md-end"> 
+            <div class="col-md-6 mb-3 text-md-end">
                 <a href="voto_delegado.php" class="btn btn-outline-dark">Delegado CG29</a>
                 <a class="btn btn-outline-dark me-2" href="alterar-senha.html">Alterar minha senha</a>
                 <a href="" class="btn btn-outline-dark" onclick="window.print()">Imprimir Resultados</a>
@@ -29,27 +29,9 @@
         </div>
 
         <div class="row">
-            <div class="col-md-6">
+            <div class="container">
                 <div class="area">
                     <?php include 'votacao.php'; ?>
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="area">
-                    <?php include 'resultado-votacao.php'; ?>
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="area">
-                    <?php include 'observacao.php'; ?>
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="area">
-                    <?php include 'dados-votacao.php'; ?>
                 </div>
             </div>
         </div>
