4f50a0414d078b0a4e96ecf0f5d3baa264f65a08
felip-araujo <felipearaujoexecutive@gmail.com>
2024-04-08 07:55:02 -0400

    ar

--- php/autenticar.php
+++ php/autenticar.php
@@ -1,37 +1,35 @@
 <?php
-
 // Iniciar a sessão
 session_start();
 
 require 'conexao.php';
 
-
-if (isset($_POST['enviar']))
+if (isset($_POST['enviar'])) {
     $email = $_POST['email'];
-$senha = $_POST['senha'];
+    $senha = $_POST['senha'];
 
-$qr = $pdo->prepare('SELECT * FROM usuarios WHERE email = :email');
-$qr->execute(['email' => $email]);
-$usuario = $qr->fetch(PDO::FETCH_ASSOC);
+    $qr = $pdo->prepare('SELECT u.*, f.nome as funcao FROM usuarios u LEFT JOIN usuario_funcao uf ON u.id = uf.usuario_id LEFT JOIN funcoes f ON uf.funcao_id = f.id WHERE u.email = :email');
+    $qr->execute(['email' => $email]);
+    $usuario = $qr->fetch(PDO::FETCH_ASSOC);
 
-// echo ($usuario['id']);
-
-if ($email === 'admin@isma.org.br' && $usuario && $usuario['senha'] === $senha) {
-    $_SESSION['autenticado'] = true;
-    $_SESSION['nivel_acesso'] = 'admin';
-    header('Location: admin-dash.php');
-} else {
     if ($usuario && $usuario['senha'] === $senha) {
-        // Autenticação bem-sucedida, definir variável de sessão
         $_SESSION['autenticado'] = true;
         $_SESSION['usuario_id'] = $usuario['id'];
 
-        // Redirecionar para a página do dashboard  
-        header('Location: dashboard.php'); // Ajuste o caminho para o dashboard
-        exit;
+        // Verificar se o usuário é um admin
+        if ($usuario['funcao'] === 'admin') {
+            $_SESSION['nivel_acesso'] = 'admin';
+            header('Location: admin-dash.php');
+            exit;
+        } else {
+            // Usuário comum
+            header('Location: dashboard.php'); // Ajuste o caminho para o dashboard do usuário comum
+            exit;
+        }
     } else {
         // Exibir mensagem de erro
-        echo "<script>alert('E-mail ou Senha Incorretos')</script>";
+        echo "<script>alert('E-mail ou Senha Incorretos');</script>";
         echo "<script>window.location.href = '../index.html';</script>";
     }
 }
+?>
